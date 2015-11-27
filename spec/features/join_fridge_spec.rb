8c16bc1d73f12e85fb5d588a7c90ab7c69dae5b4
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2015-11-27 14:24:19 +0100

    my fridges imprementation + spec

--- spec/features/join_fridge_spec.rb
+++ spec/features/join_fridge_spec.rb
@@ -5,31 +5,29 @@ describe 'all_users'  do
 
   describe 'authenticate user' do
     before(:each) do
-      user = users(:user1)
-      login_as user, scope: :user
+      @user = users(:user1)
+      login_as @user, scope: :user
     end
 
-    xit 'can see my fridges' do
+    it 'can see my fridges' do
       visit '/'
       click_link 'My fridges'
 
       expect(page).to have_content 'Moja lodówka'
       expect(page).to have_content 'Super lodówka'
+      expect(page).not_to have_content 'El Passion lodówka mała'
     end
 
     it 'can add fridge via frides view' do
-      visit "fridges/#{fridges(:fridge4).id}"
+      visit 'fridges/4'
 
       click_link 'Join fridge'
 
-      # expect(current_path).to eq '/fridges'
       expect(page).to have_content 'El Passion lodówka mała'
     end
 
-    xit 'cant join to fridge that already joined' do
-      visit "fridges/#{fridges(:fridge1).id}"
-      puts fridges(:fridge1).inspect
-      puts fridges(:fridge1).users
+    it 'cant join to fridge that already joined' do
+      visit 'fridges/1'
 
       expect(page).not_to have_content 'Join fridge'
     end
